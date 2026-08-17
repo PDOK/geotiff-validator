b9c05846cb6c347b56bff72498ad1fcc34cae5d8
Léon van der Kaap <leonkaap@gmail.com>
2026-08-17 14:20:26 +0200

    Added github integrations
    Added github workflows
    Added test skeleton
    Added validation

--- geotiff_validator/validations/__init__.py
+++ geotiff_validator/validations/__init__.py
@@ -3,7 +3,10 @@ from geotiff_validator.validations.validator import Validator
 from geotiff_validator.validations.cog_check import CogValidator
 from geotiff_validator.validations.compression_check import CompressionValidator
 
+from geotiff_validator.validations.views_check import ViewsValidator
+
 __all__ = [
     "CogValidator",
-    "CompressionValidator"
+    "CompressionValidator",
+    "ViewsValidator"
 ]
---
+++ geotiff_validator/validations/views_check.py
@@ -0,0 +1,19 @@
+from geotiff_validator.validations import validator
+from typing import Iterable
+
+class ViewsValidator(validator.Validator):
+    """The GeoTiff must not have views"""
+
+    code = 3
+    message = "GeoTiff has views"
+
+    def check(self) -> Iterable[str]:
+        bands = self.dataset_header_info.get("bands", None)
+        if bands is None:
+            return []
+        for band in bands:
+            overviews = band.get("overviews", None)
+            if overviews is not None:
+                return [f"GeoTiff has unexpected views present on bands, example: band '{band.get("band")}'"]
+
+        return []
